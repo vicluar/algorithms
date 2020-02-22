1354aa9e904c0a43cf4bfbec0e505e078e28bcbb
Vicluar Daniel Merchan <vicluar@gmail.com>
2020-02-22 21:00:13 +0100

    Creating basic elements for string builder implementation.

---
+++ Algorithms/Algorithms/Implementations/MyOwnStringBuilder/VicDStringBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Implementations.MyOwnStringBuilder
+{
+    public class VicDStringBuilder
+    {
+        public void Append(string text)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
